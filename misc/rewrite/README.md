7140b079fd342cf3eec52f696d176c94fe6635fd
taskpul <romtietz@gmail.com>
2026-01-16 12:35:25 +0100

    Delete misc directory
    Signed-off-by: taskpul <romtietz@gmail.com>

--- misc/rewrite/README.md
+++
@@ -1,5 +0,0 @@
-# OpenRewrite Recipes
-
-## Remove star/wildcard imports
-
-Run `nostarimports.sh` to remove star/wildcard imports. This should not usually be needed if IDE is configured correctly.
--- misc/rewrite/nostarimports.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-cd $(dirname $0)/../../
-
-./mvnw org.openrewrite.maven:rewrite-maven-plugin:run \
-  -Drewrite.activeRecipes=org.openrewrite.java.RemoveUnusedImports \
-  -Drewrite.activeStyles=org.keycloak.NoStarImports \
-  -Drewrite.configLocation=misc/rewrite/nostarimports.yml
