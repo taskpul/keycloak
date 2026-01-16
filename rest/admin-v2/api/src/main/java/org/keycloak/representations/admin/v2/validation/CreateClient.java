0ca053d5548cb6e1f30e215abc0d061d241a89d0
taskpul <romtietz@gmail.com>
2026-01-16 12:36:17 +0100

    Delete rest directory
    Signed-off-by: taskpul <romtietz@gmail.com>

--- rest/admin-v2/api/src/main/java/org/keycloak/representations/admin/v2/validation/CreateClient.java
+++
@@ -1,5 +0,0 @@
-package org.keycloak.representations.admin.v2.validation;
-
-// Jakarta Validation Group - validation is done only when creating a client
-public interface CreateClient {
-}
--- rest/admin-v2/api/src/main/java/org/keycloak/services/error/ViolationExceptionResponse.java
+++
@@ -1,6 +0,0 @@
-package org.keycloak.services.error;
-
-import java.util.Set;
-
-public record ViolationExceptionResponse(String error, Set<String> violations) {
-}
